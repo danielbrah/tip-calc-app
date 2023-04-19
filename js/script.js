6feecaad5564552a590c6846d433d363eb73f036
Daniel <daniel.jimenezfrancisco@gmail.com>
2023-04-19 16:18:55 -0500

    Added percentage button actions.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const percBtns = [...document.querySelectorAll('.percentage-btn')]
+
+percBtns.forEach(element => {
+    element.addEventListener('click', () =>{
+        const checkBtns = percBtns.filter(element => element.classList.contains('active'))
+
+        if(element.classList.contains('active')) return
+        else
+        {
+            if(checkBtns.length == 0) element.classList.add('active')
+            else
+            {
+                checkBtns[0].classList.remove('active')
+                element.classList.add('active')
+            }
+        }
+    })
+})
