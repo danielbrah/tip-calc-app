7c5d400e9e6853cb78323c82e6120244803bfc9a
Daniel <daniel.jimenezfrancisco@gmail.com>
2023-09-06 11:50:50 -0500

    Update.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
 const percBtns = [...document.querySelectorAll('.percentage-btn'), document.getElementById('custom-input')]
 const tip = document.getElementById('tip')
 const total = document.getElementById('total')
+const person = document.getElementById('input-person')
+const bill = document.getElementById('input-bill')
+const percInput = document.getElementById('custom-input')
+
+const calcTotal = function(){
+    const percBtnCheck = percBtns.every(element => element.classList.contains('active'));
+
+    if(!Boolean(validateInput(bill) || !Boolean(percBtnCheck) || !Boolean(validateInput(person)))) return
+    else
+    {
+
+    }
+
+}
+
+const validateInput = function(input){
+    return Number(input.value.replace(' ', '').trim())
+}
 
 percBtns.forEach(element => {
     element.addEventListener('click', () =>{
-        const checkBtns = percBtns.filter(element => element.classList.contains('active'))
-
         if(element.classList.contains('active')) return
         else
         {
-            if(checkBtns.length == 0) element.classList.add('active')
-            else
-            {
-                checkBtns[0].classList.remove('active')
-                element.classList.add('active')
-            }
+           percBtns.map(element => element.classList.remove('active'))
+           element.classList.add('active')
         }
+
+        calcTotal()
     })
+})
+
+document.addEventListener('keydown', () => {
+    if(percInput.classList.contains('active')) setTimeout(() => console.log(Boolean(validateInput(percInput))))
 })
